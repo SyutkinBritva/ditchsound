463b4550039350b2c5ea82b70821b32ff798bca2
Syutkin Britva <igorvesenniy@gmail.com>
2022-11-22 18:32:43 +0400

    Первый коммит приложения

---
+++ src/main/java/ru/ditchsound/catalog/CatalogApplication.java
@@ -0,0 +1,13 @@
+package ru.ditchsound.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogApplication.class, args);
+	}
+
+}
